bf396748eb15f55aa82149f2fea6a9d901b7eaf1
Petr Škoda <skodapetr@gmail.com>
2022-03-16 10:21:24 +0100

    Update push-annotations

--- push-annotations/README.md
+++ push-annotations/README.md
@@ -1,14 +1,7 @@
 ## Configuration
-
+ * annotations-file - Path to JSON file with annotations.
+ * title - *OPTIONAL* Title of the check.
 
 ## Input file format
 The input file should be JSON lines, i.e. one JSON object on each line.
-Each JSON should have following properties:
- * path
- * start_line
- * end_line
- * start_column
- * end_column
- * annotation_level
- * message
- * title
+Each JSON represent a single annotation. See [Properties of the annotations items](https://docs.github.com/en/rest/reference/checks).
--- push-annotations/dist/index.js
+++ push-annotations/dist/index.js
@@ -467,6 +467,7 @@ exports.issueCommand = issueCommand;
 /***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
 // Relevant repositories:
+// https://docs.github.com/en/rest/reference/checks
 // https://github.com/yuzutech/annotations-action
 
 const fileSystem = __webpack_require__(747);
@@ -474,26 +475,47 @@ const readline = __webpack_require__(58);
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-(function () {
-  const inputFile = process.env.HOME + "/" + core.getInput("annotations-file");
-  const token = process.env.GITHUB_TOKEN;
-  run(inputFile, token).catch((err) => {
-    core.setFailed(`Action failed with error ${err}`);
-    console.log(err);
-  });
+const LEVEL_NOTICE = "notice";
+const LEVEL_WARNING = "warning";
+const LEVEL_FAILURE = "failure";
+
+(async function () {
+  try {
+    await main();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 })();
 
-async function run(inputFile, token) {
+async function main() {
+  const [inputFile, token, title] = readInputs();
   const annotations = await loadAnnotationFile(inputFile);
-  const batches = createBatches(50, annotations)
-  await submitReport(batches, token);
-  checkForFailures(annotations);
+  const levels = collectLevels(annotations);
+  // The API limits upload to 50 annotations at a time.
+  const batches = createBatches(annotations, 50)
+  const octokit = github.getOctokit(token);
+  const checkRunId = await createCheck(octokit, title);
+  const conclusion = createConclusion(levels);
+  const summary = createSummary(levels);
+  for (const batch of batches) {
+    await updateCheck(octokit, checkRunId, conclusion, summary, batch);
+  }
+}
+
+function readInputs() {
+  const annotationFile = core.getInput("annotations-file", { required: true });
+  const inputFile = process.env.HOME + "/" + annotationFile;
+  const token = process.env.GITHUB_TOKEN;
+  const title = core.getInput("title");
+  return [inputFile, token, title];
 }
 
 async function loadAnnotationFile(path) {
   const readInterface = readline.createInterface({
-    input: fileSystem.createReadStream(path),
-    crlfDelay: Infinity // \r\n as a single line break
+    "input": fileSystem.createReadStream(path),
+    // Reader will wait for given time after CR is detected before, 
+    // considering next LF as a next line.
+    "crlfDelay": Infinity,
   });
   const result = [];
   for await (const line of readInterface) {
@@ -502,49 +524,56 @@ async function loadAnnotationFile(path) {
   return result;
 }
 
-const createBatches = (size, inputs) => inputs.reduce((batches, input) => {
-  const current = batches[batches.length - 1];
-  current.push(input);
-  if (current.length === size) {
-    batches.push([]);
-  }
-  return batches;
-}, [[]]);
+function createBatches(inputs, size) {
+  return inputs.reduce((batches, input) => {
+    const current = batches[batches.length - 1];
+    current.push(input);
+    if (current.length === size) {
+      batches.push([]);
+    }
+    return batches;
+  }, [[]])
+}
 
-async function submitReport(batches, token) {
-  const octokit = github.getOctokit(token);
-  const checkRunId = await createCheck(octokit, github);
-  for (const batch of batches) {
-    await octokit.checks.update({
-      "owner": github.context.repo.owner,
-      "repo": github.context.repo.repo,
-      "check_run_id": checkRunId,
-      "status": "completed",
-      "output": {
-        "title": `Check Run: ${checkRunId}`,
-        "summary": `${batch.length} errors(s) found`,
-        "annotations": batch,
-      },
-    });
+function collectLevels(annotations) {
+  const result = {
+    [LEVEL_NOTICE]: 0,
+    [LEVEL_WARNING]: 0,
+    [LEVEL_FAILURE]: 0,
+  };
+  for (const report of annotations) {
+    result[report.annotation_level] += 1;
   }
+  return result;
 }
 
 /**
  * Create a check that we can use to add annotation into.
  */
-const createCheck = async (octokit, github) => {
-  const headRef = getHeadRef(github.context);
-  const {data: {id: checkRunId}} = await octokit.checks.create({
-    "owner": github.context.repo.owner,
-    "repo": github.context.repo.repo,
-    "name": github.context.workflow,
-    "head_sha": headRef,
-    "status": "in_progress",
-  });
-  return checkRunId;
+async function createCheck(octokit, userTitle) {
+  const { owner, repo, workflow } = github.context.repo;
+  const name = isEmpty(userTitle) ? workflow : userTitle;
+  const headSha = getHeadRef(github.context);
+  try {
+    const { data: { id: checkRunId } } = await octokit.checks.create({
+      "owner": owner,
+      "repo": repo,
+      "name": name,
+      "head_sha": headSha,
+      "status": "in_progress",
+    });
+    return checkRunId;
+  } catch (err) {
+    throw new Error(
+      `Can't create a check to '${owner}/${repo}:${headSha}' - cause: ${err}`);
+  }
 };
 
-const getHeadRef = (context) => {
+function isEmpty(value) {
+  return value === null || value === undefined || value === "";
+}
+
+function getHeadRef(context) {
   if (context.payload.pull_request) {
     return context.payload.pull_request.head.sha;
   } else {
@@ -552,17 +581,42 @@ const getHeadRef = (context) => {
   }
 };
 
-function checkForFailures(annotations) {
-  let failureDetected = false;
-  for (const report of annotations) {
-    if (report.annotation_level === "failure") {
-      failureDetected = true;
-      break;
-    }
+function createConclusion(levels) {
+  if (levels[LEVEL_FAILURE] > 0) {
+    return "failure";
+  } else if (levels[LEVEL_WARNING] > 0 || levels[LEVEL_NOTICE > 0]) {
+    return "neutral";
+  } else {
+    return "success";
   }
-  if (failureDetected) {
-    core.setFailed(
-      "Some report are of type 'failure' see above logs for more details.");
+}
+
+function createSummary(levels) {
+  return `${levels[LEVEL_FAILURE]} failure(s) found\n`
+    + `${levels[LEVEL_WARNING]} warning(s) found\n`
+    + `${levels[LEVEL_NOTICE]} notice(s) found\n`;
+}
+
+async function updateCheck(
+  octokit, checkRunId, conclusion, summary, annotations,
+) {
+  try {
+    await octokit.checks.update({
+      "owner": github.context.repo.owner,
+      "repo": github.context.repo.repo,
+      "check_run_id": checkRunId,
+      "status": "completed",
+      "conclusion": conclusion,
+      "output": {
+        "title": `Check Run ${checkRunId}`,
+        "summary": summary,
+        "annotations": annotations,
+      },
+    });
+  }
+  catch (err) {
+    throw new GitHubApiError(
+      `Can't update check {check_run_id: ${checkRunId}} - cause: ${err}`);
   }
 }
 
